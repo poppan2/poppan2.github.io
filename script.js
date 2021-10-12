a5a4c4edb299fc76391dc31c61268cdfd205a270
poppan2 <ye_aung94@yahoo.com>
2021-10-11 17:44:54 -0700

    update

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ $('.toggle-button').on('click', function(){
     })
     console.log("hello")
 })
+
+$(document).ready(function(){
+
+    $('.project-photo').hover(
+        function(){
+            $(this).animate({
+                marginTop: '-=10%',
+                width: '170px',
+                height: '170px',
+            })
+        }, 
+
+        function(){
+            $(this).animate({
+                marginTop: '0%',
+                width: '150px',
+                height: '150px',
+            })
+        }, 
+    )
+})

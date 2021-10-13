9fc162b94f412734143a153f5928170c4a66edd3
poppan2 <ye_aung94@yahoo.com>
2021-10-12 21:56:16 -0700

    updated

--- script.js
+++ script.js
@@ -6,40 +6,20 @@ $('.toggle-button').on('click', function(){
 })
 
 
-// $(document).ready(function(){
-//     $('.project-photo').hover(
-//         function(){
-//             $(this).animate({
-//                 marginTop: '-=10%',
-//                 width: '170px',
-//                 height: '170px',
-//             })
-//         }, 
-
-//         function(){
-//             $(this).animate({
-//                 marginTop: '0%',
-//                 width: '150px',
-//                 height: '150px',
-//             })
-//         }, 
-//     )
-// })
-
 // Changing html when widow size changes
 $(document).ready(function(){
     $(window).on('resize', function(){
-        var win = $(this);
+        let win = $(this);
         if(win.width() >= 992){
             $('.skill-icon').removeClass('fa-5x')
             $('.skill-icon').addClass('fa-7x')
             $('.project-photo').hover(
                 function(){
                     $(this).animate({
-                        marginTop: '-=10%',
+                        marginTop: '-=5%',
                          width: '170px',
                         height: '170px',
-                    })
+                    }, 200)
                 }, 
                 
                 function(){
@@ -47,7 +27,7 @@ $(document).ready(function(){
                         marginTop: '0%',
                         width: '150px',
                         height: '150px',
-                    })
+                    },200)
                 }, 
             )
         }

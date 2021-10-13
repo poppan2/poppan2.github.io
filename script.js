8906054bf6f83e088a5f692c7ff19bf412f22347
poppan2 <ye_aung94@yahoo.com>
2021-10-13 11:15:05 -0700

    update

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-console.log("JS works")
-$('.toggle-button').on('click', function(){
-    $('ul').toggle(200, function(){
-        $('.toggle-button').toggle()
-    })
-})
+// console.log("JS works")
+// $('.toggle-button').on('click', function(){
+//     $('ul').toggle(200, function(){
+//         $('.toggle-button').toggle()
+//     })
+// })
 
 
-// Changing html when widow size changes
-$(document).ready(function(){
-    $(window).on('resize', function(){
-        let win = $(this);
-        if(win.width() >= 992){
-            $('.skill-icon').removeClass('fa-5x')
-            $('.skill-icon').addClass('fa-7x')
-            $('.project-photo').hover(
-                function(){
-                    $(this).animate({
-                        marginTop: '-=5%',
-                         width: '170px',
-                        height: '170px',
-                    }, 200)
-                }, 
+// // Changing html when widow size changes
+// $(document).ready(function(){
+//     $(window).on('resize', function(){
+//         let win = $(this);
+//         if(win.width() >= 992){
+//             $('.skill-icon').removeClass('fa-5x')
+//             $('.skill-icon').addClass('fa-9x')
+//             $('.project-photo').hover(
+//                 function(){
+//                     $(this).animate({
+//                         marginTop: '-=5%',
+//                          width: '170px',
+//                         height: '170px',
+//                     }, 200)
+//                 }, 
                 
-                function(){
-                    $(this).animate({
-                        marginTop: '0%',
-                        width: '150px',
-                        height: '150px',
-                    },200)
-                }, 
-            )
-        }
-    })
+//                 function(){
+//                     $(this).animate({
+//                         marginTop: '0%',
+//                         width: '150px',
+//                         height: '150px',
+//                     },200)
+//                 }, 
+//             )
+//         }
+//     })
 
-})
+// })
 
 
